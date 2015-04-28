e1271a43158f13144a7b3a4eff816aea6e819e35
David Alsbury <david@alsbury.com>
2015-04-27 18:25:56 -0700

    Improving privisioning script

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,12 +1,13 @@
 apt-get -y update
-apt-get -y -q install php5-fpm nginx php5 php5-json php5-gd php5-mysql php5-cli mysql-server-5.6
+apt-get -y -q install php5-fpm nginx php5 php5-json php5-gd php5-mysql php5-cli
 rm /etc/nginx/sites-enabled/default
 cp /vagrant/vagrant/files/mysql_default_charset.cnf /etc/mysql/conf.d/default_charset.cnf
 cp /vagrant/vagrant/files/nginx_server.conf /etc/nginx/sites-available/default
 ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
-sed -i "s/user www-data;/user vagrant;/" /etc/httpd/conf/httpd.conf
-
+sed -i "s/user www-data;/user vagrant;/" /etc/nginx/nginx.conf
+sed -i "s/user = www-data/user = vagrant/" /etc/php5/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = vagrant/" /etc/php5/fpm/pool.d/www.conf
 
 service nginx restart
 service php5-fpm restart

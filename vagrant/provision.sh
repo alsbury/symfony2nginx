6cefc0f3ef59e451bbae8610559dd72f4f2a0dea
David Alsbury <david@alsbury.com>
2015-04-27 19:46:10 -0700

    Working MySQL install

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,5 +1,10 @@
 apt-get -y update
-apt-get -y -q install php5-fpm nginx php5 php5-json php5-gd php5-mysql php5-cli
+
+export DEBIAN_FRONTEND=noninteractive
+echo "mysql-server-5.6 mysql-server/root_password_again password sandbox" | debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password password sandbox" | debconf-set-selections
+
+apt-get -y -q install php5-fpm nginx php5 php5-json php5-gd php5-mysql php5-cli mysql-server-5.6
 rm /etc/nginx/sites-enabled/default
 cp /vagrant/vagrant/files/mysql_default_charset.cnf /etc/mysql/conf.d/default_charset.cnf
 cp /vagrant/vagrant/files/nginx_server.conf /etc/nginx/sites-available/default
